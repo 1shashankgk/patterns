5b412b7b596cbab5d2e5a70672848246ec9adcdf
shashank gk <71203986+1shashankgk@users.noreply.github.com>
2021-12-21 19:14:38 +0530

    Create mountain2.cpp

---
+++ mountain2.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int n,i,j;
+    cin>>n;
+    for(i=1;i<=n;i++)
+    {
+        //space
+        int space =1;
+        while(space<i)
+        {
+            cout<<" ";
+            space++;
+        }
+        //star
+        for(j=n;j>=i;j--)
+            cout<<"* ";
+        cout<<endl;
+       
+    }
+
+    return 0;
+}
+/*
+
+n=6
+* * * * * * 
+ * * * * * 
+  * * * * 
+   * * * 
+    * * 
+     *
+*/
