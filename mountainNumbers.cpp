fd577f88a03dbfafd5fbaebc5d4645537dbf4791
shashank gk <71203986+1shashankgk@users.noreply.github.com>
2021-12-22 11:41:30 +0530

    Create mountainNumbers.cpp

---
+++ mountainNumbers.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int count=1,n,i,j;
+    cin>>n;
+    for(i=1;i<=n;i++)
+    {
+        //space
+        int space = n-i;
+        while(space--){
+            cout<<" ";
+        }
+        //star
+        for(j=1;j<=i;j++)
+            cout<<j;
+        
+        //another triangle
+        
+        int start = i-1;
+        while(start)
+        {
+            cout<<start--;
+        }
+        cout<<endl;
+        
+    }
+
+    return 0;
+}
+
+/*
+     1
+    121
+   12321
+  1234321
+ 123454321
+12345654321
+      
+*/
